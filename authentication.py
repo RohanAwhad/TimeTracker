9a7e1862b29a834905eaf40b766e094a74ffd0a9
Rohan Awhad <rohanavad007@gmail.com>
2020-12-12 15:34:43 +0530

    Sort of did some work. but still haven't even completed one project yet

--- authentication.py
+++ authentication.py
@@ -1,2 +1,15 @@
-def signup():
-    return 1
+import bcrypt
+
+def signup(first_name, last_name, email_id, password, username):
+  
+  salt = bcrypt.gensalt()
+  hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
+  
+  return {
+      "first_name" : first_name,
+      "last_name" : last_name,
+      "email_id" : email_id,
+      "hashed_password" : hashed_password,
+      "username" : username
+  }
+  
--- tests/test_signup.py
+++ tests/test_signup.py
@@ -1,6 +1,41 @@
+import bcrypt
 import pytest
 
-from authentication import signup
+from authentication import signup, login
 
-def test_function_exists():
-        assert signup() == 1
+def test_signup_return():
+  expected_op = {"first_name" : "Rohan", 
+                "last_name" : "Awhad", 
+                "email_id" : "rohanavad007@gmail.com", 
+                "username" : "rohanawhad",
+                "password" : "123"
+              }
+  actual_op = signup(first_name = expected_op['first_name'],
+                     last_name = expected_op['last_name'],
+                     email_id = expected_op['email_id'],
+                     password = expected_op['password'],
+                     username = expected_op['username']
+                    )
+  for k, v in actual_op.items():
+    if k == "hashed_password" : 
+      assert bcrypt.checkpw(expected_op["password"].encode("utf-8"), v)
+    else:
+      assert expected_op[k] == v
+      
+def test_signup_db_check():
+  """
+  Test if data is stored in DB
+  """
+  expected_op = {"first_name" : "Rohan", 
+                "last_name" : "Awhad", 
+                "email_id" : "rohanavad007@gmail.com", 
+                "username" : "rohanawhad",
+                "password" : "123"
+              }
+  _ = signup(first_name = expected_op['first_name'],
+                     last_name = expected_op['last_name'],
+                     email_id = expected_op['email_id'],
+                     password = expected_op['password'],
+                     username = expected_op['username']
+                    )
+  assert False
