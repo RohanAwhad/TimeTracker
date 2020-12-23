a68d02c6c5908773406c29590b1b2b79e811165e
Rohan Awhad <rohanavad007@gmail.com>
2020-12-23 09:33:51 +0530

    working

--- server/tests/test_users.py
+++ server/tests/test_users.py
@@ -1,1 +1,13 @@
 from src.models.user import User
+
+user_data = {
+    "first_name": "Max",
+    "last_name": "Payne",
+    "email_id": "max.payne_21@rockstar.com",
+    "username": "maxpayne_021",
+    "password": "Max@123",
+}
+
+
+def test_create_user():
+    max_payne = User(**user_data)
