0aa54102003e7d5ded6d841242f4ffa36144e84f
Rohan Awhad <rohanavad007@gmail.com>
2020-12-23 09:27:17 +0530

    working

--- server/src/models/users.py
+++ server/src/models/users.py
@@ -2,7 +2,7 @@ class User:
     def __init__(first_name, last_name, email_id, password, username):
         self.set_user(first_name, last_name, email_id, password, username)
 
-    def set_user(self, first_name, last_name, email_id, password, username)
+    def set_user(self, first_name, last_name, email_id, password, username):
         self._first_name = first_name
         self._last_name = last_name
         self._email_id = email_id
