7cd4ea1e020e618245c68bb54860530eae30dc1d
Rohan Awhad <rohanavad007@gmail.com>
2020-12-23 10:41:24 +0530

    working

--- server/src/hasher.py
+++ server/src/hasher.py
@@ -2,5 +2,12 @@ import bcrypt
 
 
 class Hasher:
-    def __init__():
+    def __init__(self):
         pass
+    
+    def hash(self, string_to_hash):
+    import bcrypt
+
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(string_to_hash.encode("utf-8"), salt)
+    return hashed_password.decode()
