77c1bda0eab980f9c7e8cb961c797e852f50ba98
Rohan Awhad <rohanavad007@gmail.com>
2021-03-30 21:49:26 +0530

    added time functionality. Next up is saving these 3 values in firestore. Then showing them in history. Then hosting it on firebase

---
+++ frontend/js/home.js
@@ -0,0 +1,36 @@
+var start_secs = null;
+var end_secs = null;
+var activity_name = "";
+
+$("#start_timer").click(evt => {
+    start_secs = Math.floor(Date.now() / 1000)
+    console.log(start_secs)
+})
+
+$("#end_timer").click(evt => {
+    if (start_secs !== null){
+        end_secs = Math.floor(Date.now() / 1000)
+        console.log(end_secs)
+
+        activity_name = $("#activity").val()
+        if (activity_name !== ""){
+            start = new Date(start_secs * 1000).toLocaleTimeString()
+            end = new Date(end_secs * 1000).toLocaleTimeString()
+        
+            $('#from_span').val(start)
+            $('#to_span').val(end)
+            $('#activity_span').val(activity_name)
+        }
+        else {
+            alert('Add activity name before ending timer')
+        }
+    }
+})
+
+$("#save").click(evt => {
+    // start = new Date(start_secs * 1000).toLocaleTimeString()
+    // end = new Date(end_secs * 1000).toLocaleTimeString()
+    // console.log("from ", start, "to", end, activity_name)
+    start_secs = null
+    end_secs = null
+})
